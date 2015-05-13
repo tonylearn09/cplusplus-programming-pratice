3a5002a772d20eb3479886de284d6efacd477c9d
tonylearn09 <tonyhung09@gmail.com>
2015-05-13 22:43:33 +0800

    finish chapter 0 exercise

---
+++ ch0/0-4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "// a small c++ program" << std::endl
+        << "#include <iostream>" << std::endl
+        << "\n"
+        "int main()\n"
+        "{\n"
+        "\tstd::cout << \"Hello, world!\" << std::endl;\n"
+        "\treturn 0;\n"
+        "}"
+        << std::endl;
+
+    return 0;
+
+
+}
